dcd0e5aa4eab36108363089b085f42f92f26bbcd
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-17 23:40:59 +0530

    adding components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,99 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { motion,useScroll,useSpring,useTransform } from 'framer-motion'
+import { useEffect, useState,useRef } from 'react';
+import { followPointer } from './components/followPointer';
 
 export default function Home() {
+  const style = "h-20 w-20 bg-white m-10 inline-block text-black text-center";
+
+  const list=["one","two","three","four","five"];
+  const [on, setOn] = useState(false);
+
+  const { scrollYProgress } = useScroll();
+  const rotation = useTransform(scrollYProgress, [0, 1], [0, 360]);
+
+  const ref = useRef(null);
+  const { x, y } = followPointer(ref);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className='wrapper m-10'>
+
+      <div className=' text-4xl '>Easy</div>
+          <motion.div  className={style}
+            whileHover={{ rotate:90 }}
+            whileTap={{ rotate:-90 }}
+          > Rotate </motion.div>
+
+          <motion.div  className={style}
+            whileHover={{ scale: 1.5 }}
+            whileTap={{ scale: 0.7 }}
+            transition={{
+              type:"spring",
+              stiffness:400,
+            }}
+          > Resize </motion.div>
+
+          <motion.div className={style}
+            whileHover={{ backgroundColor:'#0565ff' }}
+            whileTap={{ backgroundColor:'#ff0505' }} 
+          > Color </motion.div>
+
+          <motion.div className={style}
+            whileHover={{ outline:"solid white 5px",border:"solid black 5px" }}
+            whileTap={{ backgroundColor:'#ff0505' }} 
+          > Border </motion.div>
+
+          <motion.div className={style}
+            whileHover={{ opacity:0 }}
+            whileTap={{ opacity:1 }} 
+          > Opacity </motion.div>
+
+          <motion.div className={style}
+            whileHover={{ borderRadius:"100%" }}
+            whileTap={{borderRadius:0 }} 
+          > Radius </motion.div> 
+
+          <motion.div className={style}
+            whileHover={{ y:100 }}
+            whileTap={{ y:-100 }} 
+          > Move </motion.div> 
+
+          <motion.div className={style}
+            drag 
+          > Drag </motion.div>    
+
+
+      <div className=' text-4xl '>Medium</div>    
+          <motion.div className={style}
+            ref={ref}
+            whileHover={{ x:x, y:y}}
+            drag 
+          > follow </motion.div> 
+
+          <motion.div className={style}
+          > background-position </motion.div>
+          
+          <motion.div className={style}
+              style={{ rotate:rotation }}
+          > scroll </motion.div>
+
+          <motion.div className={style}
+              style={{ scaleX: scrollYProgress }}
+          > drawing border </motion.div>
+
+          <motion.div className={style}
+          > drawing border </motion.div>
+
+          <motion.ul className={`h-min w-20 bg-white m-10 flex text-black text-center ${on?"flex-col-reverse":"flex-col"}`}
+            transition= {{
+              staggerChildren: 0.5
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+              <motion.button layout onClick={() => setOn(!on)} className='bg-blue-500'>order</motion.button>
+             {list.map((value,key)=>{
+                return (<motion.li key={key} layout>{value}</motion.li>)
+             })}
+          </motion.ul>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
